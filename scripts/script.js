a1e8f004a1376af69fe88883d41e5de8e2a84f37
Makee Ham <79068658+makee-ham@users.noreply.github.com>
2025-05-16 21:32:31 +0900

    ✨ #3 카드 데이터에 이미지 경로 매핑 및 가공 함수 구현
    * ✨ #3 이미지 url을 매핑하는 함수 getCardImgUrl 모듈 생성
    
    utils 디렉토리를 별도로 만들어 images.js에 getCardImgUrl 함수 만듦. 데이터 형식을 파일명 형식에 맞게 맞춰주는 함수 makeDataValueFit도  해당 모듈에 넣어둠
    
    * ✨ #3 fetch한 카드객체 각각에 image property map함
    
    script.js의 fetch 라인 아래에 getCardImgUrl을 통해 각 카드 객체에 image 프로퍼티를 맵한 배열 cards 만듦
    
    Closes #3

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,18 @@
 import { fetchTarotData } from "./tarot-api";
 import { cardLoadingStart, cardLoadingOver } from "./ui";
 
+import { getCardImgUrl } from "./utils/images";
+
 async function prepareDrawingCards() {
   cardLoadingStart();
   try {
     const tarotData = await fetchTarotData();
     // console.log(tarotData);
+
+    const cards = tarotData.map((card) => ({
+      ...card,
+      image: getCardImgUrl(card),
+    }));
   } catch (err) {
     console.error(err);
     alert(err.message);
