aa06fb5a47ab2ef87334d9d24fda240aef8abcfb
Makee <ina0312@naver.com>
2025-05-16 22:04:33 +0900

    ✨ #5 prepareDrawingCards에 카드셔플 적용
    페치해온 타로데이터배열에 방금 만든 함수를 적용하도록 함

--- scripts/script.js
+++ scripts/script.js
@@ -2,17 +2,20 @@ import { fetchTarotData } from "./tarot-api";
 import { cardLoadingStart, cardLoadingOver } from "./ui";
 
 import { getCardImgUrl } from "./utils/images";
+import { shuffleCards } from "./utils/shuffle";
 
 async function prepareDrawingCards() {
   cardLoadingStart();
   try {
     const tarotData = await fetchTarotData();
     // console.log(tarotData);
-
-    const cards = tarotData.map((card) => ({
+    const shuffledCards = shuffleCards(tarotData);
+    const cards = shuffledCards.map((card) => ({
       ...card,
       image: getCardImgUrl(card),
     }));
+
+    // 이제 여기서 cards를 뒷면으로 렌더하게...
   } catch (err) {
     console.error(err);
     alert(err.message);
