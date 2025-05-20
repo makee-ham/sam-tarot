df7aee1f14d1603105c309e153e106710a7f37b3
Makee <ina0312@naver.com>
2025-05-20 18:27:48 +0900

    tailwind와 animate 제반 환경 설정

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#845EC2",
+        secondary: "#D65DB1",
+        accent: "#FF9671",
+        bg: "#2C2C54",
+        surface: "#4B4453",
+        text: "#F8F8F8",
+      },
+    },
+  },
+};
